9325a0ee282b18b71bbdbbd83a8ee626b9041bc6
zwomac1 <zachary.womack10@gmail.com>
2022-10-11 15:53:29 +0000

    Initial commit

---
+++ src/app.config.js
@@ -0,0 +1,10 @@
+/**
+ * Place to store data specific to this instance
+ * DO NOT store secrets here
+ * DO NOT store state here - must be part of a stateless service
+ */
+class AppConfig {
+    team = 'Team 7';
+    version = '1.0.1';
+}
+module.exports = AppConfig;
